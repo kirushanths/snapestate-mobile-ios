f76b5e363082405e38fba907d0fe3edb014851af
KirushanthS <kirushanth.2@gmail.com>
2014-04-21 07:51:49 -0400

    started on tab bar redesign

---
+++ snapestate-mobile-ios/classes/controllers/SETabBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  SETabBarViewController.h
+//  snapestate-mobile-ios
+//
+//  Created by Kirushanth on 2014-03-15.
+//  Copyright (c) 2014 SnapEstate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SETabBarViewController : UITabBarController
+
+@end
---
+++ snapestate-mobile-ios/classes/controllers/SETabBarViewController.m
@@ -0,0 +1,35 @@
+//
+//  SETabBarViewController.m
+//  snapestate-mobile-ios
+//
+//  Created by Kirushanth on 2014-03-15.
+//  Copyright (c) 2014 SnapEstate. All rights reserved.
+//
+
+#import "SETabBarViewController.h"
+
+@interface SETabBarViewController ()
+
+@end
+
+@implementation SETabBarViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+@end
